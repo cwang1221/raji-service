fd6a0a3edd4c04eae516c4405f423e14e12bd7be
I322871 <wangcong891221@163.com>
2021-05-21 23:59:54 +0800

    chore: enable cors for localhost:3000

--- src/services/express.js
+++ src/services/express.js
@@ -23,7 +23,10 @@ export default function (apiRoot, routes) {
   }
 
   if (env === 'production' || env === 'development') {
-    app.use(cors())
+    app.use(cors({
+      origin: 'http://localhost:3000',
+      credentials : true
+    }))
     app.use(compression())
     app.use(morgan('dev'))
   }
